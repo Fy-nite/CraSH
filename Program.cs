fd425a308e2d152508eb8b02c56210ff1fcb9192
charlie-sans <gamerboy030607@gmail.com>
2026-02-02 01:46:30 +1100

    meow

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using System;
+using Adamantite.VFS;
+using StarChart.AppFramework;
+using StarChart.PTY;
+
+namespace CraSH
+{
+	internal static class Program
+	{
+		static void Main(string[] args)
+		{
+			// Setup a simple in-memory VFS and mount it at root
+			var memFs = new InMemoryFileSystem();
+			memFs.CreateDirectory("");
+			memFs.WriteAllBytes("welcome.txt", System.Text.Encoding.UTF8.GetBytes("Welcome to CraSH!\n"));
+
+			var vfsManager = new VfsManager();
+			vfsManager.Mount("/", memFs);
+			VFSGlobal.Manager = vfsManager;
+
+			var pty = new ConsolePty();
+			var shellApp = new StarChart.AppFramework.ShellApp(pty, vfsManager);
+
+			var ctx = new StarChart.Plugins.PluginContext { VFS = vfsManager, Arguments = args, WorkingDirectory = "/", PrimaryPty = pty };
+
+			// Run using AppHost (will return when the shell exits)
+			AppHost.Run(shellApp, ctx);
+
+			// exit process when shell returns
+			return;
+		}
+	}
+}
